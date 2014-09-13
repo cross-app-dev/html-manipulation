8321175c08e219fc080279b4a73a29995de75ffd
Wael Showair <show0017@algonquinlive.com>
2014-09-12 21:06:17 -0400

    initial commit

---
+++ js/manipulation.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+
+window.onload = init;
+
+function init( ){
+	//this function runs after the page loads
+	//Add listener for click on the Article Heading
+		
+}
+
+function stepTwo( ){
+	//this function runs when the user clicks on the Article heading
+	//Add listener for mouseover the second paragraph on the page
+	
+}
+
+function stepThree( ){
+	//this function runs when the user mouses over the second paragraph
+	//Add listener for mouseout on the Instructions heading
+	
+}
+
+function stepFour( ){
+	//this function runs when the user mouses out of the Instructions heading
+	//Add listener for click on the paragraph in the footer
+	
+}
+
+function complete( ){
+	//this runs after the user clicks on the paragraph in the footer
+		
+}
